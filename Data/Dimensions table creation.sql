f3fd6ec6a35976412ece6cb251e1320a66085ef6
Gnana Priyanka Yelchuri <gnanapriya415@gmail.com>
2026-06-11 11:45:32 +0530

    Update Dimensions table creation.sql

--- Data/Dimensions table creation.sql
+++ Data/Dimensions table creation.sql
@@ -1,3 +1,5 @@
+-- Laptop Dimension Table Creation
+
 create table dim_laptop
 (
 Laptop_id int identity(1,1) primary key,
@@ -6,10 +8,14 @@ Price int,
 Rating float
 );
 
+-- Brand Dimension Table Creation
+
 create table dim_brand(
 Brand_id int identity(1,1) primary key,
 Brand nvarchar(50)
 );
+
+-- Processor Dimension Table Creation
 
 create table dim_processor(
 Processor_id int identity(1,1)primary key,
@@ -25,11 +31,15 @@ Energy_Efficient_Units smallint,
 Threads float
 );
 
+-- RAM Dimension Table Creation
+
 create table dim_ram(
 Ram_id int identity(1,1) primary key,
 [RAM_GB] tinyint,
 [RAM_type] nvarchar(50)
 );
+
+-- Storage Dimension Table Creation
 
 create table dim_storage(
 Storage_id int identity(1,1) primary key,
@@ -39,6 +49,9 @@ Storage_capacity_GB nvarchar(50)
 alter table [dbo].[dim_storage]
 add [Storage_type] nvarchar(50);
 ---------------
+
+-- Graphics Dimension Table Creation
+
 create table dim_graphics(
 Graphics_id int identity(1,1) primary key,
 Graphics_name nvarchar(50),
@@ -46,6 +59,8 @@ Graphics_name nvarchar(50),
 [Graphics_GB] float,
 [Graphics_integreted] bit
 );
+
+-- Display Dimension Table Creation
 
 create table dim_display(
 display_id int identity(1,1) primary key,
@@ -55,10 +70,14 @@ display_id int identity(1,1) primary key,
 [ppi] float
 );
 
+-- Touch Screen Dimension Table Creation
+
 create table dim_touchScreen(
 [Touch_screen_id] int identity(1,1) primary key,
 [Touch_screen] bit
 );
+
+-- Operating System Dimension Table Creation
 
 create table dim_OS(
 OS_id int identity(1,1) primary key,
