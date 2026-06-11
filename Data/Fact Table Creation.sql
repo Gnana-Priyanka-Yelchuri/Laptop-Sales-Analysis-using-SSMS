f5478ba5569096fe0b0b2c0f585f1852a151bc3d
Gnana Priyanka Yelchuri <gnanapriya415@gmail.com>
2026-06-11 12:39:37 +0530

    Create Fact Table Creation.sql

---
+++ Data/Fact Table Creation.sql
@@ -0,0 +1,21 @@
+create table fact_laptop
+(
+fact_ID int identity(1,1) primary key,
+[Brand_id] int, 
+[display_id] int, 
+[Graphics_id] int, 
+[Laptop_id] int,
+[OS_id] int, 
+[Processor_id] int, 
+[Ram_id] int, 
+[Storage_id] int, 
+[Touch_screen_id] int,
+
+foreign key ([Brand_id]) references [dbo].[dim_brand] ([Brand_id]),
+foreign key ([display_id]) references [dbo].[dim_display] ([display_id]),
+foreign key ([Graphics_id]) references [dbo].[dim_graphics] ([Graphics_id]),
+foreign key ([Laptop_id]) references [dbo].[dim_laptop] ([Laptop_id]),
+foreign key ([OS_id]) references [dbo].[dim_OS] ([OS_id]),
+foreign key ([Processor_id]) references [dbo].[dim_processor] ([Processor_id]),
+foreign key ([Ram_id] )references [dbo].[dim_ram] ([Ram_id])
+);
